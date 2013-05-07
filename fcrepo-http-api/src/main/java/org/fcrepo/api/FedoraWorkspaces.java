a0b190d7fc6f1870153d4047976aca23daeaa105
fasseg <frank.asseg@googlemail.com>
2013-05-07 14:06:02 +0200

    added Resource and a simple test for crud operations on workspaces. Still unbale to delete a workspace, and merge hast still to be tested with some new nodes added or old ones removed

---
+++ fcrepo-http-api/src/main/java/org/fcrepo/api/FedoraWorkspaces.java
@@ -0,0 +1,74 @@
+package org.fcrepo.api;
+
+import javax.inject.Inject;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.RepositoryFactory;
+import javax.jcr.Session;
+import javax.jcr.Workspace;
+import javax.jcr.version.VersionManager;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.fcrepo.AbstractResource;
+import org.modeshape.jcr.JcrRepositoryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/rest/fcr:workspaces")
+public class FedoraWorkspaces extends AbstractResource {
+	
+	@Autowired
+	private RepositoryFactory jcrFactory;
+	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response createWorkspace() throws RepositoryException {
+		final Session sess = getAuthenticatedSession();
+		String name = RandomStringUtils.randomAlphanumeric(16);
+		sess.getWorkspace().createWorkspace(name);
+		sess.save();
+		return Response.status(201).entity(name).build();
+	}
+
+	@GET
+	@Path("{name}")
+	@Produces({MediaType.APPLICATION_JSON,MediaType.TEXT_XML})
+	public Response getWorkspace(@PathParam("name") final String name) throws RepositoryException {
+		final Session sess = getAuthenticatedSession();
+		final Session wsSess = sess.getRepository().login(name);
+		String current = wsSess.getWorkspace().getName();
+		return Response.ok(current).build();
+	}
+	
+	
+	@DELETE
+	@Path("{name}")
+	public Response deleteWorkspace(@PathParam("name") final String name) throws RepositoryException {
+		final Session sess = getAuthenticatedSession();
+		if (name.equals("default")){
+			throw new RepositoryException("Unable to delete the default workspace");
+		}
+		sess.getWorkspace().deleteWorkspace(name);
+		sess.save();
+		return Response.ok().build();
+	}
+
+	@POST
+	@Path("/fcr:merge/{name}")
+	public Response mergeWorkspace(@PathParam("name") final String name) throws RepositoryException {
+		final Session sess = getAuthenticatedSession();
+		Workspace ws1 = sess.getWorkspace();
+		VersionManager vm = ws1.getVersionManager();
+		vm.merge("/", name, false);
+		return Response.ok().build();
+	}
+}
---
+++ fcrepo-http-api/src/test/java/org/fcrepo/integration/api/FedoraWorkspacesIT.java
@@ -0,0 +1,58 @@
+package org.fcrepo.integration.api;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.junit.Test;
+
+public class FedoraWorkspacesIT extends AbstractResourceIT {
+
+	@Test
+	public void testWorkspaceCreate() throws Exception {
+		HttpPost createWs = new HttpPost(serverAddress + "fcr:workspaces");
+		HttpResponse resp = execute(createWs);
+		assertTrue(resp.getStatusLine().getStatusCode() == 201);
+		String name = IOUtils.toString(resp.getEntity().getContent());
+		assertTrue("invalid name returned: " + name, name.length() == 16);
+
+		HttpGet getWs = new HttpGet(serverAddress + "fcr:workspaces/" + name);
+		resp = execute(getWs);
+		assertTrue(resp.getStatusLine().getStatusCode() == 200);
+		assertEquals(name, IOUtils.toString(resp.getEntity().getContent()));
+	}
+
+	@Test
+	public void testWorkspaceDelete() throws Exception {
+		HttpPost createWs = new HttpPost(serverAddress + "fcr:workspaces");
+		HttpResponse resp = execute(createWs);
+		assertTrue(resp.getStatusLine().getStatusCode() == 201);
+		String name = IOUtils.toString(resp.getEntity().getContent());
+		assertTrue("invalid name returned: " + name, name.length() == 16);
+
+		HttpDelete rmWs = new HttpDelete(serverAddress + "fcr:workspaces/" + name);
+		resp = execute(rmWs);
+		assertTrue(resp.getStatusLine().getStatusCode() == 200);
+
+		/* TODO: this should fail but the repo still returns the Workspace after the deletion */
+		HttpGet getWs = new HttpGet(serverAddress + "fcr:workspaces/" + name);
+		resp = execute(getWs);
+		assertTrue("Repository returned: " + resp.getStatusLine(), resp.getStatusLine().getStatusCode() == 404);
+	}
+
+	@Test
+	public void testWorkspaceMerge() throws Exception {
+		HttpPost createWs = new HttpPost(serverAddress + "fcr:workspaces");
+		HttpResponse resp = execute(createWs);
+		assertTrue(resp.getStatusLine().getStatusCode() == 201);
+		String name = IOUtils.toString(resp.getEntity().getContent());
+		assertTrue("invalid name returned: " + name, name.length() == 16);
+
+		HttpPost mergeWs = new HttpPost(serverAddress + "fcr:workspaces/fcr:merge/" + name);
+		resp = execute(mergeWs);
+		assertTrue("Repository returned: " + resp.getStatusLine(), resp.getStatusLine().getStatusCode() == 200);
+	}
+}
