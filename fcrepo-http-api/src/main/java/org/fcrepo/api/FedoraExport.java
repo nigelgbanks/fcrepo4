de54a4516f663e5d377202aabec76a6f59b43131
ajs6f <ajs6f@virginia.edu>
2013-05-08 14:06:38 -0400

    Changed FedoraExport to use injected Session

--- fcrepo-http-api/src/main/java/org/fcrepo/api/FedoraExport.java
+++ fcrepo-http-api/src/main/java/org/fcrepo/api/FedoraExport.java
@@ -22,12 +22,18 @@ import javax.ws.rs.core.StreamingOutput;
 
 import org.fcrepo.AbstractResource;
 import org.fcrepo.serialization.FedoraObjectSerializer;
+import org.fcrepo.session.InjectedSession;
 import org.slf4j.Logger;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 @Path("/rest/{path: .*}/fcr:export")
 public class FedoraExport extends AbstractResource {
+
+    @InjectedSession
+    private Session session;
 
     @Resource
     private Map<String, FedoraObjectSerializer> serializers;
@@ -35,37 +41,43 @@ public class FedoraExport extends AbstractResource {
     private final Logger logger = getLogger(this.getClass());
 
     @GET
-    public StreamingOutput exportObject(@PathParam("path") final List<PathSegment> pathList, @QueryParam("format")
+    public StreamingOutput exportObject(@PathParam("path")
+    final List<PathSegment> pathList, @QueryParam("format")
     @DefaultValue("jcr/xml")
     final String format) {
-		final String path = toPath(pathList);
+        final String path = toPath(pathList);
 
         logger.debug("Requested object serialization for: " + path +
                 " using serialization format " + format);
 
-		return new StreamingOutput() {
-			@Override
-			public void write(final OutputStream out) throws IOException {
-				final Session session = getAuthenticatedSession();
-				try {
-					logger.debug("Selecting from serializer map: " +
-							serializers);
-					final FedoraObjectSerializer serializer =
-							serializers.get(format);
-					logger.debug("Retrieved serializer for format: " + format);
-					serializer.serialize(objectService.getObject(session, path), out);
-					logger.debug("Successfully serialized object: " + path);
-				} catch (final RepositoryException e) {
-					throw new WebApplicationException(e);
-				} finally {
-					session.logout();
-				}
-			}
-		};
+        return new StreamingOutput() {
+
+            @Override
+            public void write(final OutputStream out) throws IOException {
+                try {
+                    logger.debug("Selecting from serializer map: " +
+                            serializers);
+                    final FedoraObjectSerializer serializer =
+                            serializers.get(format);
+                    logger.debug("Retrieved serializer for format: " + format);
+                    serializer.serialize(
+                            objectService.getObject(session, path), out);
+                    logger.debug("Successfully serialized object: " + path);
+                } catch (final RepositoryException e) {
+                    throw new WebApplicationException(e);
+                } finally {
+                    session.logout();
+                }
+            }
+        };
     }
 
     public void setSerializers(
             final Map<String, FedoraObjectSerializer> serializers) {
         this.serializers = serializers;
+    }
+
+    public void setSession(final Session session) {
+        this.session = session;
     }
 }
